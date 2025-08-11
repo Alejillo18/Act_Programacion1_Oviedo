a83be4b63024e5c61ad62a148488eda6b8b17f1c
Alejillo18 <alejoemmanueloviedo@gmail.com>
2025-08-11 19:36:39 -0300

    Act 11-08

---
+++ Act-11-08_Oviedo/main.js
@@ -0,0 +1,10 @@
+let a = 10*10**5;
+let b = 3*3;
+
+
+console.log("suma", a + b)
+console.log("resta", a - b)
+console.log("multiplicacion", a * b)
+console.log("division", a / b)
+console.log("Modulo", a % b)
+console.log("Exponente", a ** 3)
