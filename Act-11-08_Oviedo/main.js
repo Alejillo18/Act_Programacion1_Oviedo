0c3bb08c6c87fac0142292980cc920339f5884e1
Alejillo18 <alejoemmanueloviedo@gmail.com>
2025-08-11 20:53:14 -0300

    Act-final11-08

--- Act-11-08_Oviedo/main.js
+++ Act-11-08_Oviedo/main.js
@@ -8,3 +8,38 @@ console.log("multiplicacion", a * b)
 console.log("division", a / b)
 console.log("Modulo", a % b)
 console.log("Exponente", a ** 3)
+
+a = 20*3*258
+
+
+a % 2 === 0 ? alert(`${a} Es par`) : alert(`${a} Es impar`)
+
+a = 5;
+b = "5";
+
+
+console.log( a == b) // true
+console.log(a === b) //false
+console.log(a != b) //false
+console.log(a !== b) //false
+
+
+console.log( a  >  b )
+console.log(a < b)
+
+let resultado = a === 5 ? "Es 5" : "No es 5"
+console.log(resultado)
+
+//Operador ternario
+
+let x = true;
+let y = false;
+console.log( x && y) // false
+console.log(x || y) //true
+console.log(!x) // false
+console.log(!!!a) //false
+
+/* const h1 = document.createElement("h1")
+h1.textContent = "Hola manito"
+h1.style.fontSize = "clamp(0.3125rem, 0.3125rem + 2.15vw, 3rem)"
+document.body.appendChild(h1) */
